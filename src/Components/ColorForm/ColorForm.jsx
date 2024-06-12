6b8d2bf7c62a8d323af099bfb036d9cf48ae6413
Phillip Oehlenberg <hi@phillip-oehlenberg.com>
2024-06-12 11:17:06 +0200

    Add first inputs to ColorForm

---
+++ src/Components/ColorForm/ColorForm.jsx
@@ -0,0 +1,14 @@
+export default function ColorForm() {
+  return (
+    <form>
+      <div>
+        <label htmlFor="colorName">Role</label>
+        <input id="colorName" type="text" />
+      </div>
+      <div>
+        <label htmlFor="hex">Role</label>
+        <input id="hex" type="text" />
+      </div>
+    </form>
+  );
+}
