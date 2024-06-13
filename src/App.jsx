e66ca3aa8d8fac477b20ec29a8ff0cccdbdf56bf
Phillip Oehlenberg <hi@phillip-oehlenberg.com>
2024-06-13 11:45:19 +0200

    Complete delete color

--- src/App.jsx
+++ src/App.jsx
@@ -20,12 +20,18 @@ function App() {
     setColors([newColor, ...colors]);
   }
 
+  function handleDeleteColor(id) {
+    setColors(colors.filter((color) => color.id !== id));
+  }
+
   return (
     <>
       <h1>Theme Creator</h1>
       <ColorForm onAddColor={handleAddColor} />
       {colors.map((color) => {
-        return <Color key={color.id} color={color} />;
+        return (
+          <Color key={color.id} color={color} onDelete={handleDeleteColor} />
+        );
       })}
     </>
   );
--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,6 +1,13 @@
 import "./Color.css";
+import { useState } from "react";
 
-export default function Color({ color }) {
+export default function Color({ color, onDelete }) {
+  const [isDelete, setIsDelete] = useState(false);
+
+  function handleToggleDelete() {
+    isDelete ? setIsDelete(false) : setIsDelete(true);
+  }
+
   return (
     <div
       className="color-card"
@@ -9,9 +16,30 @@ export default function Color({ color }) {
         color: color.contrastText,
       }}
     >
-      <h3 className="color-card-headline">{color.hex}</h3>
+      <h3 className="color-card-hightlight">{color.hex}</h3>
       <h4>{color.role}</h4>
       <p>contrast: {color.contrastText}</p>
+      {!isDelete ? (
+        <button type="button" onClick={handleToggleDelete}>
+          DELETE
+        </button>
+      ) : (
+        <>
+          <p className="color-card-hightlight">Are you sure?</p>
+          <button type="button" onClick={handleToggleDelete}>
+            CANCEL
+          </button>
+
+          <button
+            type="button"
+            onClick={() => {
+              onDelete(color.id);
+            }}
+          >
+            DELETE
+          </button>
+        </>
+      )}
     </div>
   );
 }
